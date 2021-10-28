2ac023346e9cb6f74bdad1c8cacb47df710d3ec3
exploring-curiosity <curiositywake@gmail.com>
2021-10-28 10:09:31 +0530

    3D Scene Lighting and animation done

--- SourceFiles/EX10/Source.cpp
+++ SourceFiles/EX10/Source.cpp
@@ -1,37 +1,11 @@
-/*#include<gl/glut.h>
-#include<iostream>
-using namespace std;
-
-void init() {
-	glClearColor(1, 1, 1, 1);
-	glOrtho(-500, 500, -500, 500, -500, 500);
-	glEnable(GL_DEPTH_TEST);
-}
-
-void display() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glColor3f(1, 0, 0);
-	glutWireCube(100);
-	glFlush();
-}
-
-int main(int argc, char* argv[]) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(1000, 1000);
-	glutCreateWindow("Lighting and animation");
-	glutDisplayFunc(display);
-	init();
-	glutMainLoop();
-	return 1;
-}
-*/
 #include<gl/glut.h>
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int alpha = 45, theta = 45;
+GLfloat alpha = 0, theta = 0;
 bool flag = true;
+vector<vector<GLfloat>> coords(8, vector<GLfloat>(3));
 
 void init(void)
 {
@@ -44,11 +18,12 @@ void init(void)
 	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
 	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_DEPTH_TEST);
 }
+
+
 
 void display(void)
 {
@@ -57,26 +32,35 @@ void display(void)
 	glLoadIdentity();
 	glOrtho(-500, 500, -500, 500, -500.0, 500.0);
 	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
 	glRotatef(alpha, 0, 1, 0);
 	glRotatef(theta, 1, 0, 0);
-	glutSolidCube(100);
+	//glShadeModel(GL_SMOOTH);
+	glutSolidTeapot(100);
 	glFlush();
+}
+
+void timer(int v) {
+	alpha += 0.2;
+	theta += 0.2;
+	if (alpha == 360) alpha = 0;
+	if (theta == 360) theta = 0;
+	glutPostRedisplay();
+	glutTimerFunc(100, timer, v);
 }
 
 
 void keyPress(int key, int x, int y) {
 	switch (key) {
-	case GLUT_KEY_RIGHT: alpha++;
+	case GLUT_KEY_RIGHT: alpha+=1;
 		break;
 	case GLUT_KEY_LEFT:
-		alpha--;
+		alpha-=1;
 		break;
 	case GLUT_KEY_UP:
-		theta++;
+		theta+=1;
 		break;
 	case GLUT_KEY_DOWN:
-		theta--;
+		theta-=1;
 		break;
 	case GLUT_KEY_HOME: flag = !flag;
 		break;
@@ -94,6 +78,7 @@ int main(int argc, char** argv)
 	init();
 	glutDisplayFunc(display);
 	glutSpecialFunc(keyPress);
+	glutTimerFunc(100,timer,0);
 	glutMainLoop();
 	
 	return 0;
