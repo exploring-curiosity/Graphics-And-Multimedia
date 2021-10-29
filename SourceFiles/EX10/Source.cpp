fca0c1a6688cbb489237c05264ee005cc9eaf45b
exploring-curiosity <curiositywake@gmail.com>
2021-10-29 19:09:09 +0530

    Ex10 Lighting and animation of 4 objects done

--- SourceFiles/EX10/Source.cpp
+++ SourceFiles/EX10/Source.cpp
@@ -3,15 +3,16 @@
 #include<vector>
 using namespace std;
 
-GLfloat alpha = 0, theta = 0;
-bool flag = true;
+GLfloat alpha = 300, theta = 300, gamma = 300;
+int index=0;
+bool rev=false;
 vector<vector<GLfloat>> coords(8, vector<GLfloat>(3));
-
+GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
 void init(void)
 {
 	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
 	GLfloat mat_shininess[] = { 50.0 };
-	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+	
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glShadeModel(GL_SMOOTH);
 
@@ -30,45 +31,39 @@ void display(void)
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-500, 500, -500, 500, -500.0, 500.0);
+	gluPerspective(100, 1, 0.1, 10000);
 	glMatrixMode(GL_MODELVIEW);
-	glRotatef(alpha, 0, 1, 0);
-	glRotatef(theta, 1, 0, 0);
-	//glShadeModel(GL_SMOOTH);
+	glLoadIdentity();
+	glRotatef(30, 0, 1, 0);
+	gluLookAt(gamma, alpha, theta, -500, 0, -500, 0, 1, 0);
+	glutSolidCube(100);
+	gluLookAt(gamma, alpha, theta, 500, 0, -500, 0, 1, 0);
 	glutSolidTeapot(100);
+	gluLookAt(gamma, alpha, theta, -500, 0, 500, 0, 1, 0);
+	glutSolidSphere(100,20,20);
+	gluLookAt(gamma, alpha, theta, 500, 0, 500, 0, 1, 0);
+	glutSolidTorus(50,100,20,20);
+	gluLookAt(gamma, alpha, theta, 1000, 0, 0, 0, 1, 0);
 	glFlush();
 }
 
-void timer(int v) {
-	alpha += 0.2;
-	theta += 0.2;
-	if (alpha == 360) alpha = 0;
-	if (theta == 360) theta = 0;
-	glutPostRedisplay();
-	glutTimerFunc(100, timer, v);
-}
-
-
-void keyPress(int key, int x, int y) {
-	switch (key) {
-	case GLUT_KEY_RIGHT: alpha+=1;
-		break;
-	case GLUT_KEY_LEFT:
-		alpha-=1;
-		break;
-	case GLUT_KEY_UP:
-		theta+=1;
-		break;
-	case GLUT_KEY_DOWN:
-		theta-=1;
-		break;
-	case GLUT_KEY_HOME: flag = !flag;
-		break;
+void timer(int v)
+{
+	if (!rev) {
+		alpha += 1;
+		theta += 1;
+		gamma += 1;
+		if (alpha == 500) rev = true;
+	}
+	else {
+		alpha -= 1;
+		theta -= 1;
+		gamma -= 1;
+		if (alpha == 200) rev = false;
 	}
 	glutPostRedisplay();
+	glutTimerFunc(10, timer, v);
 }
-
-
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
@@ -77,8 +72,7 @@ int main(int argc, char** argv)
 	glutCreateWindow("3D Scene");
 	init();
 	glutDisplayFunc(display);
-	glutSpecialFunc(keyPress);
-	glutTimerFunc(100,timer,0);
+	glutTimerFunc(10,timer,0);
 	glutMainLoop();
 	
 	return 0;
